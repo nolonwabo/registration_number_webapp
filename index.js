e4c66613029869e7be67337feb54765affbdf10c
Nolonwabo <nolonwabomahanjana13@gmail.com>
2017-08-23 16:57:54 +0200

    Added a filter

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var exphbs = require('express-handlebars');
-var schema = require('./model');
+var mongoose = require('mongoose');
+var model = require('./model');
 app.use(bodyParser.urlencoded({
   extended: false
 }));
@@ -13,17 +14,51 @@ app.engine('handlebars', exphbs({
   defaultLayout: 'main'
 }));
 app.set('view engine', 'handlebars');
-var server = app.listen(3001);
 
 app.get('/', function(req, res) {
   var regNum = req.body.regNum;
   res.render('index', {});
 });
-var storeRegistration =[];
+
+var storeRegistration = [];
 app.post('/reg_numbers', function(req, res) {
   var regNum = req.body.regNum;
-  storeRegistration.push(regNum);
-  res.render('index', {
-    numberPlates: storeRegistration
+  var storingRegPlates = new model.storeRegNum({
+    regNum: regNum
   });
+  storeRegistration.push(regNum);
+
+  storingRegPlates.save(function(err) {
+    if (err) {
+      console.log('Error  Message: ' + err);
+    } else {
+      console.log('Save to database');
+      res.render('index', {
+        numberPlates: storeRegistration
+      });
+    }
+  });
+
+});
+app.post('/filter', function(req, res) {
+  var town = req.body.town;
+  console.log(town);
+  model.storeRegNum.find({
+    regNum: {
+      '$regex': '.*' + town
+    }
+  }, function(err, plates) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(plates);
+      res.render('index', {
+        regPlates: plates
+      })
+    }
+  })
+})
+var port = process.env.PORT || 3001
+var server = app.listen(port, function() {
+  console.log("Started app on port : " + port)
 });
--- model.js
+++ model.js
@@ -1,8 +1,11 @@
 var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
 const MongoURL =process.env.MONGO_DB_URL || "mongodb://localhost/registration";
 mongoose.connect(MongoURL, {
   useMongoClient: true
 });
+
+
 exports.storeRegNum = mongoose.model('storeRegNum', {
   regNum: String
  });
