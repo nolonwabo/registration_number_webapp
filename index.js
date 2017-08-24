d6be0742ae97c07b2252cec55fdb7bb9246d4761
Nolonwabo <nolonwabomahanjana13@gmail.com>
2017-08-24 12:24:03 +0200

    cleaned my code

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var exphbs = require('express-handlebars');
-var mongoose = require('mongoose');
 var model = require('./model');
 app.use(bodyParser.urlencoded({
   extended: false
--- model.js
+++ model.js
@@ -1,5 +1,4 @@
 var mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
 const MongoURL =process.env.MONGO_DB_URL || "mongodb://localhost/registration";
 mongoose.connect(MongoURL, {
   useMongoClient: true
